fdfd0f1bfee825b8e548ad0dffb898cb0ca3b119
munenari <munenari@gmail.com>
2018-05-22 22:20:05 +0900

    Add other options in command line param

--- README.md
+++ README.md
@@ -15,10 +15,29 @@ docker run -it --rm -v `pwd`:/data puppeteer \
 	print-to-pdf -f /etc/hostname -o /data/output.pdf --scale 2
 ```
 
+```bash
+docker run -it --rm -v `pwd`:/data puppeteer \
+	print-to-pdf -u https://example.com -o /data/output.pdf
+```
+
 ### script example
 
+from file
+
 ```
-echo '<meta charset="utf8">今日はいい天気ですね' | print-to-pdf -f '/dev/stdin'
+echo '<meta charset="utf8">今日はいい天気ですね' | ./print-to-pdf.js -f '/dev/stdin'
+```
+
+from url (file: scheme is also available)
+
+```
+./print-to-pdf.js -u 'https://www.google.com'
+```
+
+when you are using macOS, -c option may help you
+
+```
+./print-to-pdf.js -u 'https://www.google.com' -c /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome
 ```
 
 ## available options
@@ -26,16 +45,43 @@ echo '<meta charset="utf8">今日はいい天気ですね' | print-to-pdf -f '/d
 you can show this help by '-h' or '--help' option
 
 + -f, --file [string]
+
 input html file path
+> file value will be overrided by url value
+
++ -u, --url [string]
+
+input html url path
+> url value will override file path value
 
 + -o, --output [string] (default: output.pdf)
+
 output file path
 
 + --format [string] (default: A4)
+
 output format type [A4, letter, or so]
 
 + --scale [integer] (default: 1)
+
 output scale factor [> 0]
 
 + --media [string] (default: screen)
+
 emulate media type [screen, media]
+
++ --landscape [boolean] (default: false)
+
+this document will be landscaped
+
++ --printBackground
+
+print background images
+
++ --displayHeaderFooter
+
+display header and footer
+
++ -c, --chromium [string] (default <alpine>: /usr/bin/chromium-browser)
+
+executable chromium path
--- print-to-pdf.js
+++ print-to-pdf.js
@@ -8,24 +8,36 @@ const chromiumBin = '/usr/bin/chromium-browser'
 
 commander
 	.option( '-f, --file <s>', 'input html file path' )
+	.option( '-u, --url <s>', 'input html url path' )
 	.option( '-o, --output <s>', 'output file path', 'output.pdf' )
 	.option( '--format <s>', 'output format type [A4, letter, or so]', 'A4' )
 	.option( '--scale <n>', 'output scale factor [> 0]', 1 )
 	.option( '--media <s>', 'emulate media type [screen, media]', 'screen' )
+	.option( '--landscape', 'this document will be landscaped' )
+	.option( '--printBackground', 'print background images' )
+	.option( '--displayHeaderFooter', 'display header and footer' )
+	.option( '-c, --chromium <s>', 'executable chromium path', chromiumBin )
 	.parse( process.argv )
 
-if ( ! commander.file ) {
+if ( ! commander.file && ! commander.url ) {
 	commander.help( )
 }
 
-const body = fs.readFileSync( commander.file, 'utf8' )
-if ( ! body || ! commander.output ) {
-	commander.help( )
+let url
+if ( commander.url ) {
+	url = commander.url
+}
+else {
+	const body = fs.readFileSync( commander.file )
+	if ( ! body ) {
+		commander.help( )
+	}
+	url = 'data:text/html;,' + encodeURIComponent( body )
 }
 
 ( async ( ) => {
 	const browser = await puppeteer.launch( {
-		executablePath: chromiumBin,
+		executablePath: commander.chromium,
 		args: [
 			'--headless',
 			'--no-sandbox'
@@ -33,15 +45,17 @@ if ( ! body || ! commander.output ) {
 	} )
 	const page = await browser.newPage( )
 	await page.emulateMedia( commander.media )
-	await page.goto( 'data:text/html;,' + encodeURIComponent( body ), {
+	await page.goto( url, {
 		waitUntil: 'networkidle2'
 	} )
+
 	await page.pdf( {
 		path: commander.output,
 		scale: parseFloat(commander.scale),
-		displayHeaderFooter: false,
+		displayHeaderFooter: commander.displayHeaderFooter,
 		format: commander.format,
-		printBackground: true
+		printBackground: commander.printBackground,
+		landscape: commander.landscape
 	} )
 	await browser.close( )
 } )( )
